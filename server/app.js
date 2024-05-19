b8a3a73d512bff9f3de10f8012a3d159401320e0
Eli4545 <elisimon4545@gmail.com>
2024-05-19 09:11:24 -0700

    created request and response function

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
+
 
 
 const app = express();
 
-app.use (express.static('public'));
+app.use ('/', express.static(path.join(__dirname + '/public')));
+
+app.use((req, res)=> {
+    res.status(404);
+    res.send(`<h1>Error 404: Page not found</h1>`);
+})
 
 app.listen(4000, () => {
     console.log("App listening on port 4000");
